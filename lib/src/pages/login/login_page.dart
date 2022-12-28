1faee8851ad3af1a3f158af4a1b1b0f2ddea3147
facubarberis <facubarberis@gmail.com>
2022-12-28 09:49:37 -0300

    fixs PROD

--- lib/src/pages/login/login_page.dart
+++ lib/src/pages/login/login_page.dart
@@ -145,7 +145,7 @@ class _LoginBodyState extends State<LoginBody> {
 //Funcion para redirigir al Link de Descarga de la Ultima Aplicacion Disponible
   _launchURL() async {
     const url =
-        'https://dh.formosa.gob.ar/modulos/webservice/php/version_3_0/v3.0.3.apk';
+        'https://dh.formosa.gob.ar/modulos/webservice/php/version_3_0/v3.0.0.apk';
     await launch(url);
   }
 }
